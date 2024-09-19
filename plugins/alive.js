bc5b41a335840bf066dd567b48d8608d8441313c
madushanrathnayakasl <jaliyamadushan21@gmail.com>
2024-09-19 22:38:02 +0530

    Create alive.js

---
+++ plugins/alive.js
@@ -0,0 +1,21 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+
+cmd({
+    pattern: "alive",
+    desc: "Check bot online or no.",
+    category: "main",
+    react: "👋",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+let des = `..............................................ALIVE NOW....................................`
+
+return await conn.sendMessage(from,{image: {url: `https://i.ibb.co/tLxNF4M/Whats-App-Image-2024-09-19-at-22-07-43-53e71399.jpg`},caption: des},{quoted: mek})
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
