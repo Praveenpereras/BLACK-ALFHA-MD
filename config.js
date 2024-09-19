52dca62b92625f8cb6bae735bc562a405349da9b
madushanrathnayakasl <jaliyamadushan21@gmail.com>
2024-09-19 22:20:03 +0530

    ◄⚚𝙱𝙻𝙰𝙲𝙺 𝙰𝙻𝙵𝙷𝙰⚚►

---
+++ config.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "put sesson id",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/p22J5nQ/file-1nh1gowb-Fqjz1-YJGzgo8u1e-J.jpg",
+SUDO_NB: process.env.SUDO_NB || "94766946854",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_BIO: process.env.AUTO_BIO || "true",
+MODE: process.env.MODE || "public",
+AUTO_VOICE:"true",
+};
