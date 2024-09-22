71d3659a1ba4370c20ea1fc62263bdee61a8e692
madushanrathnayakasl <jaliyamadushan21@gmail.com>
2024-09-23 00:45:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,10 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/tLxNF4M/Whats-App-Image-2024-09-19-at-22-07-43-53e71399.jpg",
-ALIVE_MSG: process.env.ALIVE_IMG || "🤖🔰 HEY BLACK ALFHA-MD IS ONLINE NOW💻\n*💻OWENER* - BLACK ALFHA-MD\n\n*💻OWENER NUMBER* -94762777919",
-SUDO_NB: process.env.SUDO_NB || "94766946854",
+SESSION_ID: process.env.SESSION_ID || "put sesson id",
+SUDO_NB: process.env.SUDO_NB || "94762777919",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
+AUTO_BIO: process.env.AUTO_BIO || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true"
+AUTO_VOICE:"true",
 };
