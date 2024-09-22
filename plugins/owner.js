b3d25b974b4c867babff0dd89fd51dbe0fbf5140
madushanrathnayakasl <jaliyamadushan21@gmail.com>
2024-09-23 03:45:13 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,7 +11,17 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-let dec = `.................OWENER............`
+let dec = `⚡BLACK ALFHA-MD⚡
+
+> 🌍𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 
+
+⚡ᴏᴡɴᴇʀ ɴᴀᴍᴇ -: PD 𝚖𝚘𝚍𝚣ꪻ𝐏яανι𝚈αнꪶ🕊🍒* 
+⚡ɴᴜᴍʙᴇʀ -: https://wa.me/+94762777919
+⚡ʏᴏᴜᴛᴜʙᴇ -: https://youtube.com/@madushanboy?si=qv8sq2ngTObYtapi
+⚡ᴡʜᴀᴛꜱᴀᴘᴘ bot group -: https://chat.whatsapp.com/LcYUwlY3p35E2pHKWKfaJb 
+
+ρσɯҽԃ Ⴆყ PD 𝚖𝚘𝚍𝚣ꪻ𝐏яανι𝚈αнꪶ🕊🍒`
+    
 await conn.sendMessage(from,{image:{url: `https://i.ibb.co/tLxNF4M/Whats-App-Image-2024-09-19-at-22-07-43-53e71399.jpg`},caption:dec},{quoted:mek});
 await conn.sendMessage(from, {
             audio: { url: 'https://github.com/KING-HANSA/VOICE-KING-HANSA/blob/main/Hello%20hello(tbg).mp3?raw=true' },
