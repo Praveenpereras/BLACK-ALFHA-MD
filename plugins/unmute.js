763407a29a114cde56d3b38292eef3867488c2fe
madushanrathnayakasl <jaliyamadushan21@gmail.com>
2024-09-23 03:23:07 +0530

    Create unmute.js

---
+++ plugins/unmute.js
@@ -0,0 +1,24 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+     pattern: "unmute",	
+     alias: ["unlock"],
+     react: "🔊",
+     desc: "mute group.",
+     category: "group",
+     filename: __filename,
+},
+async(conn, mek, m,{from, l, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants,  isItzcp, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+        if (!isGroup) return reply('This command can only be used in a group❗.')
+        if (!isBotAdmins) return reply('කරුනාකර BLACK ALFHA MD බොට් වෙත ඇඩ්මින් ලබා දෙන්න ❗.')
+        if (!isAdmins) return reply('ඔබ owner නොවෙ ❌.') 
+  
+            await conn.groupSettingUpdate(m.chat, "not_announcement")
+           const mass = await conn.sendMessage(m.chat, { text: '🔊 GROUP IS OPEN MY BOT OWNER' }, { quoted: mek });
+            return await conn.sendMessage(m.chat, { react: { text: '🔊', key: mass.key } });
+} catch(e) {
+console.log(e);
+reply('කරුනාකර black alfha md bot ඇඩ්මින් ලබා දෙන්න ❗❗')     
+} 
+})
